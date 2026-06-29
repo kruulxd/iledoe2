b92b1112ac10f0851826826f9d7a54c13154f6d7
kruulxd <56634090+kruulxd@users.noreply.github.com>
2026-06-29 20:05:41 +0200

    Update print statement to say 'Goodbye World'

--- za-ile-respi-addon.user.js
+++ za-ile-respi-addon.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Za ile respi Elita II & Tytan
 // @namespace    http://tampermonkey.net/
-// @version      1.7.3
+// @version      1.7.4
 // @description  Pokazuje timery elit II i tytanow z pelna integracja Lootlog
 // @author       Kruul
 // @match        https://*.margonem.pl/
